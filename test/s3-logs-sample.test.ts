ae8990246ae67275d39383291779d052111a0c18
Dan Kiuna <dkkiuna@amazon.com>
2024-04-24 11:13:55 -0700

    Initial commit

---
+++ test/s3-logs-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3LogsSample from '../lib/s3-logs-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-logs-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3LogsSample.S3LogsSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
