43b86f1fabc921504cdbcbe2d8a39d8d9fd4c74a
Dan Kiuna <dkkiuna@amazon.com>
2024-04-25 14:05:37 -0700

    initial commit

--- bin/s3-logs-sample.ts
+++ bin/s3-logs-sample.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { S3LogsSampleStack } from '../lib/s3-logs-sample-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { S3LogsSampleStack } from "../lib/s3-logs-sample-stack";
 
 const app = new cdk.App();
-new S3LogsSampleStack(app, 'S3LogsSampleStack', {
+new S3LogsSampleStack(app, "S3LogsSampleStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,10 @@ new S3LogsSampleStack(app, 'S3LogsSampleStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: {
+    // account: '123456789012',
+    region: "us-west-2",
+  },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/s3-logs-sample-stack.ts
+++ lib/s3-logs-sample-stack.ts
@@ -1,16 +1,82 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as path from "path";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class S3LogsSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, "appsyncapi", {
+      name: "myapiwiths3logs",
+      definition: appsync.Definition.fromFile(
+        path.join(__dirname, "schema.graphql")
+      ),
+      logConfig: {
+        retention: logs.RetentionDays.ONE_DAY,
+        fieldLogLevel: appsync.FieldLogLevel.ALL,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3LogsSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myBucket = new s3.Bucket(this, "s3AppSyncDatasource", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const httpDatasource = api.addHttpDataSource(
+      "json-dummy-data",
+      "https://jsonplaceholder.typicode.com"
+    );
+
+    const s3Datasource = new appsync.HttpDataSource(this, "s3Datasource", {
+      api,
+      endpoint: "https://" + myBucket.bucketRegionalDomainName,
+      authorizationConfig: {
+        signingRegion: "us-west-2",
+        signingServiceName: "s3",
+      },
+    });
+
+    myBucket.grantPut(s3Datasource.grantPrincipal);
+
+    const listPostFn = new appsync.AppsyncFunction(this, "listPostFn", {
+      dataSource: httpDatasource,
+      api,
+      name: "listPostfunction",
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      code: appsync.Code.fromAsset(
+        path.join(__dirname, "resolvers", "listPost.js")
+      ),
+    });
+
+    const s3Fn = new appsync.AppsyncFunction(this, "s3function", {
+      dataSource: s3Datasource,
+      api,
+      name: "s3Function",
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      code: appsync.Code.fromAsset(
+        path.join(__dirname, "resolvers", "s3resolver.js")
+      ),
+    });
+
+    new appsync.Resolver(this, "listpostresolver", {
+      api,
+      fieldName: "listPost",
+      typeName: "Query",
+      pipelineConfig: [listPostFn, s3Fn],
+      code: appsync.Code.fromAsset(
+        path.join(__dirname, "resolvers", "pipeline.js")
+      ),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+    });
+
+    new cdk.CfnOutput(this, "s3bucketEndpoint", {
+      value: myBucket.bucketDomainName,
+    });
+
+    new cdk.CfnOutput(this, "s3bucketRegionalEndpoint", {
+      value: myBucket.bucketRegionalDomainName,
+    });
   }
 }
